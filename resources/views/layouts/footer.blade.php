4c8c84961aa5cea70897aeb26fede9d0a40998b3
Irem Tektas <tektasirem13@gmail.com>
2022-02-25 23:22:41 +0300

    initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,40 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="  w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div class="text-center
+" >
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Sayfalar
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Anasayfa
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Giriş
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Kayıt Ol
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+
+
+
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-center text-xs text-white pt-6">
+        Bütün hakları saklıdır
+    </p>
+</footer>
